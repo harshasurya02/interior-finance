078b3511a91ad478623ef440c16b322c679fa98c
harshasurya02 <harshasurya02@gmail.com>
2025-03-09 22:41:09 +0530

    bundle analyzer updated

--- next.config.ts
+++ next.config.ts
@@ -53,4 +53,9 @@ const nextConfig: NextConfig = {
   // },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
+// export default nextConfig;
